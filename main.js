334e9eb528f95060a01c3bbc9b1e913bc484eefb
fenroe <scrambles9494@gmail.com>
2024-07-24 15:42:37 +0300

    Made a grid of squares that change colour on mouseover

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-alert("Hello world");
+const container = document.getElementById("container");
+
+for (let i = 0; i < 16; i += 1) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  container.append(square);
+  square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = "red";
+  });
+}
